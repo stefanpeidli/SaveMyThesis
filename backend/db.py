6e1761056d864637284206f16318b511fdde6d5c
dakingha69 <dongha.kim210@gmail.com>
2019-04-13 01:02:25 +0200

    Add naive db handlers
    DB getter methods should work.
    Insert method is implemented naively.
    Business logic still missing.

---
+++ backend/db.py
@@ -0,0 +1,16 @@
+
+def insert_version(collection, version):
+    inserted_doc = collection.insert_one(version)
+    return inserted_doc.inserted_id
+
+def get_version_by_id(collection, version_id):
+    doc = collection.find_one({ "_id": version_id })
+    return doc
+
+def get_history(collection):
+    docs = []
+    for doc in collection.find():
+        doc["_id"] = str(doc["_id"])
+        docs.append(doc)
+    return docs
+
