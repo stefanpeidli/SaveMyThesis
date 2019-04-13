4be1f3a52fdf30cf714dc35be1caf6ef83379662
dakingha69 <dongha.kim210@gmail.com>
2019-04-13 12:58:17 +0200

    Add production build for gh-pages

---
+++ docs/precache-manifest.831f70ea615d890a944507db7c0c00c8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "16d3afe9c7ed4fb4f56b",
+    "url": "/static/js/main.99a9bbca.chunk.js"
+  },
+  {
+    "revision": "31ccfebc8a50e8ba06f5",
+    "url": "/static/js/2.794efc9f.chunk.js"
+  },
+  {
+    "revision": "16d3afe9c7ed4fb4f56b",
+    "url": "/static/css/main.dbb6b0fa.chunk.css"
+  },
+  {
+    "revision": "0419d86d6e07da3d2c7e9dd826de72d4",
+    "url": "/index.html"
+  }
+];
