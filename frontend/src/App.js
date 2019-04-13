b679c0fc7352cf87ca21aabc57424376261f3ca2
jowet <github@jonas.mailrange.com>
2019-04-13 15:07:44 +0200

    Chatbot integration

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,34 @@ import Preview from './components/Preview';
 import DiffPreview from './components/DiffPreview';
 import History from './components/History';
 
+<<<<<<< 5079b021665295e43453e280d2e6807b7e0f8d7c
 import { postVersion, fetchHistory } from './services/versionService';
+=======
+import VersionFinderChatBot from './components/VersionFinderChatBot';
+
+import ChatBot from 'react-simple-chatbot';
+
+const mockHistory = [
+  {
+    id: 1,
+    commitTitle: 'Add paragraph about dogs',
+    timestamp: 1555083960769,
+    author: 'Dong-Ha Kim'
+  },
+  {
+    id: 2,
+    commitTitle: 'Change paragraph about cats',
+    timestamp: 1555083960769,
+    author: 'Dong-Ha Kim'
+  },
+  {
+    id: 3,
+    commitTitle: 'Remove errors',
+    timestamp: 1555083960769,
+    author: 'Dong-Ha Kim'
+  }
+]
+>>>>>>> Chatbot integration
 
 class App extends Component {
   state = {
@@ -77,6 +104,8 @@ class App extends Component {
             <History
               history={this.state.history}
               onClickItem={this.handleClickHistoryItem}
+              />
+              <VersionFinderChatBot
               />
           </Box>
         </Box>
